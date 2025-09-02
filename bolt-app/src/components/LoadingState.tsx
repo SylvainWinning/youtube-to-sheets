a1cfd4d6d86ab86145093aa7ac1fc54a290eba0d
SylvainWinning <168933918+SylvainWinning@users.noreply.github.com>
2025-09-02 02:34:24 -0700

    Reduce padding from p-8 to p-4 in LoadingState component

--- bolt-app/src/components/LoadingState.tsx
+++ bolt-app/src/components/LoadingState.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function LoadingState() {
   return (
-    <div className="flex items-center justify-center p-8">
+    <div className="flex items-center justify-center p-44
       <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
     </div>
   );
